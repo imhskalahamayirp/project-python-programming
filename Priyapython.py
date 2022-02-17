f54d14a8cdf3b69f37bbc06f46d6c13e3e507eb1
imhskalahamayirp <92021912+imhskalahamayirp@users.noreply.github.com>
2022-02-17 14:16:37 +0530

    Create Priyapython.py

---
+++ Priyapython.py
@@ -0,0 +1,6 @@
+python is a higher level programming language
+python was developed by Guido Van Rossum
+python is a open source platform
+python is a interpreted language
+python is similar to english language
+python can run in any os
